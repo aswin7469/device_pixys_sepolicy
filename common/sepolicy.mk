ArrowOS/android_device_arrow_sepolicy

sepolicy: Move LineageOS SDK policy to platform
Change-Id: Iae32c09e7c0d1dd07bc8978bd0419eeec7ed876c

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/arrow/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/arrow/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/arrow/sepolicy/common/private

aswin7469/device_pixys_sepolicy

common/sepolicy.mk
@@ -3,5 +3,8 @@
 # inherit from Lineage
 #
 
-BOARD_SEPOLICY_DIRS += \
-    device/pixys/sepolicy/common
+BOARD_PLAT_PUBLIC_SEPOLICY_DIR += \
+    device/pixys/sepolicy/common/public
+
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    device/pixys/sepolicy/common/private